839169df6099a13967e6aad8de9faaef21f99c58
Alain Tchougbo <alain.tchougbo@epitech.eu>
2024-12-10 11:29:53 +0000

    Add Acueil page file version 1

--- Frontend/afro-stream/src/App.vue
+++ Frontend/afro-stream/src/App.vue
@@ -1,12 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-</script>
-
-<template>
-  <HelloWorld />
+<!-- <template>
+       <PreAccueil />
 </template>
 
-<style scoped>
+<script setup>
+import PreAccueil from './components/PreAccueil.vue';
+// import Accueil from './components/Accueil.vue';
+</script>
 
+<style>
+
+</style> -->
+
+<template>
+  <div class=" text-2xl bg-yellow-100 flex justify-center items-center min-h-screen">
+        <PreAccueil v-if="showPreAccueil"/>
+    <Accueil v-else />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import PreAccueil from './components/PreAccueil.vue';
+import Accueil from './components/Accueil.vue';
+
+const showPreAccueil = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    showPreAccueil.value = false; // Passe à Accueil
+  }, 3000);
+});
+</script>
+
+<style>
+/* Ajoutez vos styles ici si nécessaire */
 </style>
+
---
+++ Frontend/afro-stream/src/components/PreAccueil.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-yellow-100 flex justify-center items-center min-h-screen">
+      <img 
+        src="/AfroStream.png" 
+        alt="AfroStream Logo" 
+        class=" w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+      />
+    </div>
+  </template>
+  
+  <style scoped>
+  </style>
+  
