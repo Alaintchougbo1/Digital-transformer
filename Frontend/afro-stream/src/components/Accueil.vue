c0e3e065031446799ff7100de5a00d43f42019fb
Meryprog <mereprecieux@gmail.com>
2024-12-11 00:04:50 +0100

    Component PreAccueil Update

--- Frontend/afro-stream/src/components/Accueil.vue
+++ Frontend/afro-stream/src/components/Accueil.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="bg-yellow-100 flex justify-center items-center min-h-screen">
-    <div>
-      <p class="text-lg text-gray-600 text-center">Bienvenue sur</p>
-      <img
-        src="/AfroStreamText1.png"
-        alt="Logo AfroStream"
-        class="w-48 mt-4"
-      />
-      <p class="mt-4 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </div>
+  <div class="flex flex-col justify-center items-center">
+    <!-- Welcome text -->
+    <p class="text-lg text-gray-600 text-center">Bienvenue sur</p>
+
+    <!-- Logo text with enhanced styles -->
+    <span class="text-green-900 font-bold text-4xl tracking-wide drop-shadow-lg">
+      Afro Steam
+    </span>
+
+    <!-- Description text -->
+    <p class="mt-4 text-center text-gray-700 max-w-md">
+      Les Événements qui Comptent, Toujours à Portée de Main
+    </p>
   </div>
+</div>
+
 </template>
 
 <script>
