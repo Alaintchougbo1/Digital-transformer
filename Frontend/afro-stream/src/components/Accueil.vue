ea034dc502ce63820fe46380ee2ec191611796aa
Geovic20 <kpossilandegeovic68@gmail.com>
2024-12-10 17:42:12 +0100

    Enrégistrement des modifications

--- Frontend/afro-stream/src/components/Accueil.vue
+++ Frontend/afro-stream/src/components/Accueil.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="bg-yellow-100 flex justify-center items-center min-h-screen">
         <span>
-            <p class=" text-opacity-30 justify-center">Bienvenue sur</p> 
+            <p class=" text-opacity-30 justify-center">Bienvenue sur Afro</p>
             <span>
-                <img 
-                    src="/AfroStream.png" 
-                    alt="AfroStream Logo" 
+                <img
+                    src="/AfroStream.png"
+                    alt="AfroStream Logo"
                     class=" w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
                 />
             </span>
         </span>
     </div>
-  </template>
-  
+</template>
+
   <style scoped>
   </style>
-  
